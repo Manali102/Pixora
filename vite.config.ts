2e666521d0ed2289ee56ee077aa8e4e3667c4d01
manaliqodic <manali@qodictechnosoft.com>
2026-04-07 16:28:12 +0530

    handle the versioning

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,14 @@ import tailwindcss from '@tailwindcss/vite'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Pixora/",   // ✅ VERY IMPORTANT
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
