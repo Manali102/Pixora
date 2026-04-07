310b5e1feacf14dd9cf7664ddc4960d59b5eb237
manaliqodic <manali@qodictechnosoft.com>
2026-04-07 16:53:05 +0530

    vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: "/Pixora/",   // ✅ VERY IMPORTANT
+  base: "/",
 
   plugins: [
     react(),
